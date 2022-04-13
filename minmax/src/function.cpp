77e2e0f202c3e0c73f0e25efa2106e9336cb61c8
rarysonfred700 <99487663+rarysonfred700@users.noreply.github.com>
2022-04-12 21:30:50 -0300

    Update function.cpp

--- minmax/src/function.cpp
+++ minmax/src/function.cpp
@@ -1,19 +1,20 @@
 #include "function.h"
 
-/*! 
- * Finds and returns a pair with the first instance of the smallest element
- * and the last instance of the largest element in an array.
- *
- * @param V This is the array-to-pointer decay representing an array.
- * @param n The array's size.
- *
- * @return A pair of indexes to the first smallest and last largest values.
- */
+std::pair<int,int> min_max(int vetor[], std::size_t tamanho){
+  int max = 0;
+  int min = 0;
+  pair <int, int> val;
 
-std::pair<int,int> min_max( int V[], size_t n )
-{
-    // TODO: Adicione aqui sua solução.
-
-    // TODO: Isso é apenas um STUB. Substitua com seu retorno correto.
-    return { -1, -1 };
+  for (int i = 0; i < tamanho; i++) {
+    if(vetor[i] >= vetor[max]){      
+      max = i;
+    } else{
+    }
+    if(vetor[i] < vetor[min]){
+      min = i;
+    } else{
+    }
+  }  
+  val = std::make_pair(min, max);
+  return val;
 }
