7c150e91706f45b8cc09b6ba6899ddec65f0fa12
rarysonfred700 <99487663+rarysonfred700@users.noreply.github.com>
2022-04-12 21:27:12 -0300

    Update main.cpp

--- intervalos/src/main.cpp
+++ intervalos/src/main.cpp
@@ -1,9 +1,3 @@
-/*!
- * @brief This code implements the Intervalos programming problem
- * @author selan
- * @data June, 6th 2021
- */
-
 #include <iostream>
 using std::cout;
 using std::cin;
@@ -11,11 +5,44 @@ using std::endl;
 #include <iomanip>
 using std::setprecision;
 
-// Se desejar, crie funções aqui, antes do main().
-
-int main(void)
-{
-    // TODO: Adicione aqui a sua solução.
-
-    return 0;
+int main(void){
+  int x;
+  int contador1 = 0;
+  int contador2 = 0;
+  int contador3 = 0;
+  int contador4 = 0;
+  int contador5 = 0;
+  int total = 0;
+  double z = static_cast<double>(total);
+  
+  while(cin >> std::ws >> x) {
+    if(x > 99 || x < 0){
+      contador5 = contador5 + 1;
+      z+=1;
+    }
+    if(x >= 0 && x < 25){
+      contador1 = contador1 + 1;
+      z+=1;
+    }
+    if(x > 24 && x < 50){
+      contador2 = contador2 + 1;
+      z+=1;
+    }
+    if(x > 49 && x < 75){
+      contador3 = contador3 + 1;
+      z+=1;
+    }
+    if(x > 74 && x < 100){
+      contador4 = contador4 + 1;
+      z+=1;
+    }
+  }
+  std::cout << setprecision(4);
+  std::cout << (contador1*100)/z << endl;
+  std::cout << (contador2*100)/z << endl;
+  std::cout << (contador3*100)/z << endl;
+  std::cout << (contador4*100)/z << endl;
+  std::cout << (contador5*100)/z << endl;
+  
+  return 0;
 }
