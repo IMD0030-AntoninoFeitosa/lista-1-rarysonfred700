128b265ca12fae65f19ef37a6e06979b5486e60d
rarysonfred700 <99487663+rarysonfred700@users.noreply.github.com>
2022-04-17 20:07:11 -0300

    Update function.cpp

--- inverter/src/function.cpp
+++ inverter/src/function.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <array>
 
-/*! 
- * Reverse de order of elements inside the array.
- * @param arr Reference to the array with the values.
- */
 template <std::size_t SIZE>
-void reverse( std::array< std::string, SIZE > & arr )
-{
-    /*codigo*/
+void reverse(std::array< std::string, SIZE > & arr){
+    size_t arraySizej = arr.size();
+    size_t arraySizei = arr.size();
+    
+    for(int j = 0; j < arraySizej - 1; j++){
+      for(int i = 0; i < arraySizei - 1; i++){
+        std::swap(arr[i], arr[i++]);
+      }
+      arraySizei -= 1;
+    }
 }
