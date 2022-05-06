0082350bf12e7c3b16945f73914d43e92ccdc3e0
rarysonfred700 <99487663+rarysonfred700@users.noreply.github.com>
2022-05-06 08:59:47 -0300

    Update function.cpp

--- ponto_em_retangulo_2/src/function.cpp
+++ ponto_em_retangulo_2/src/function.cpp
@@ -1,24 +1,11 @@
 #include "function.h"
 
 location_t pt_in_rect(const Ponto &IE, const Ponto &SD, const Ponto &P){
-  location_t pt;
-  if(((P.x <= IE.x && P.x >= SD.x)) || ((P.x >= IE.x) && (P.x <= SD.x))){
-    if(((P.y <= IE.y && P.y >= SD.y)) || ((P.y >= IE.y && P.y <= SD.y))){
-      if((P.x == IE.x) || (P.x == SD.x)){
-        pt = BORDER;
-      } else{
-        pt = INSIDE;    
-      }
-      if((P.y == SD.y) || (P.y == IE.y)){
-        pt = BORDER;
-      } else{
-        pt = INSIDE;  
-      }
-    } else{
-      pt = OUTSIDE;
-    }
-  } else{
-    pt = OUTSIDE;
+  location_t loc = location_t::BORDER;
+  if((P.x < IE.x) || (P.x > SD.x) || (P.y < IE.y) || (P.y <= SD.y)){
+    loc = location_t::OUTSIDE;
+  } else if((P.x > IE.x) && (P.x < SD.x) && (P.y > IE.y) && (P.y < SD.y)){
+    loc = location_t::INSIDE;
   }
-  return pt;
+  return loc;
 }
