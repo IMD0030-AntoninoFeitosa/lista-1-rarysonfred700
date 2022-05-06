46593ce122d8de18b955128518d823ca99bfa8b0
rarysonfred700 <99487663+rarysonfred700@users.noreply.github.com>
2022-05-06 08:42:13 -0300

    Update function.cpp

--- inverter/src/function.cpp
+++ inverter/src/function.cpp
@@ -3,13 +3,9 @@
 
 template <std::size_t SIZE>
 void reverse(std::array< std::string, SIZE > & arr){
-    size_t arraySizej = arr.size();
-    size_t arraySizei = arr.size();
-    
-    for(int j = 0; j < arraySizej - 1; j++){
-      for(int i = 0; i < arraySizei - 1; i++){
-        std::swap(arr[i], arr[i++]);
-      }
-      arraySizei -= 1;
+    int size = arr.size();
+
+    for(int i = 0; i < size/2; i++){
+        std::swap(arr[i], arr[size-i-1]);
     }
 }
