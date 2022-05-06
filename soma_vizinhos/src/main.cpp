2f60fdb33b6e284855b2be0c7053d9ce9716aa6c
rarysonfred700 <99487663+rarysonfred700@users.noreply.github.com>
2022-05-06 08:47:00 -0300

    Update main.cpp

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -4,32 +4,23 @@ using std::cin;
 using std::endl;
 
 int main(void){
-  int m;
-  int n;
-  int soma = 0;
-  while(1){
-    std::cin >> m;
-    std::cin >> n;
-
-    if(n == 0){
-      soma = m;
-    } else{
-    }
-    if(n < 0){
-      for(int i = m; i > m + n; i--){
-        soma += i;
+  int inicio, fim, soma = 0;
+  while(cin >> std::ws >> inicio >> fim){
+    
+    if(fim > 0){
+      for(int i = 0; i < fim; i++){
+        soma += inicio;
+        inicio++;
+      }
+    } else if(fim < 0){
+      for(int i = 0; i < -fim; i++){
+        soma += inicio;
+        inicio--;
       }
     } else{
+      soma = inicio;
     }
-    if(n > 0){
-      for(int i = m; i < m + n; i++){
-        soma += i;
-      }
-    } else{
-    }
-
     std::cout << soma << std::endl;
-    soma = 0;
   }
   return 0;
 }
